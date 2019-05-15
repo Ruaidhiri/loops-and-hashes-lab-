6e61e1fb8bd5c3e106e4e6639e5d39f1b097b7ca
John McNeill <Shetlandjohnmcneill@hotmail.co.uk>
2019-05-15 16:12:35 +0100

    That's all folks

--- my_functions.rb
+++ my_functions.rb
@@ -1,4 +1,30 @@
 def add_array_lengths (array1, array2)
-    array_plus_array = array1.length.to_i + array2.length.to_i
+  array_plus_array = array1.length.to_i + array2.length.to_i
   return array_plus_array
-end 
+end
+
+def sum_array(numbers)
+  counter = 0
+
+  for number in numbers
+    counter += number
+  end
+
+  return counter
+
+end
+
+def find_item(array, item)
+  for house in array
+    if house == item
+
+      return true
+    end
+  end
+  return false
+end
+
+def get_first_key(hash)
+ first_hash = hash.keys[0]
+    return first_hash
+end
